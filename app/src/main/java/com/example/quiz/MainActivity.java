83e569af61b60000cb18ffd53ff04a01ea6da738
br1ghtless3 <xoroshman.darya@mail.ru>
2022-05-19 23:57:04 +0700

    Initial commit

---
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final LinearLayout sssr = findViewById(R.id.sssr);
+        final LinearLayout winx = findViewById(R.id.winx);
+        final LinearLayout disney = findViewById(R.id.disney);
+
+
+        sssr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        winx.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        disney.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
+    }
+}
